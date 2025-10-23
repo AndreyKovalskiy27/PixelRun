26284ee9fdcdfeec04b5135a8ccdd74859e6ecce
AndreyKovalskiy27 <andreykovalskiy27@gmail.com>
2025-10-23 13:56:54 +0300

    Add infinity background

--- game/background.py
+++ game/background.py
@@ -13,13 +13,19 @@ class Background:
         self.surface = pygame.transform.scale(
             self.surface,
             settings.WINDOW_SIZE)  # Stretch the background to fill the entire screen
+        
+        # Second background
+        self.second_background = self.surface.copy()
 
     def draw(self, screen):
         """Draw background on the screen"""
-        screen.blit(
-            self.surface,
-            (self.x, 0)
-        )
+
+        screen.blit(self.surface, (self.x, 0))
+
+        screen.blit(self.surface, (self.x + self.surface.get_width(), 0))
+
+        if self.x <= -self.surface.get_width():
+            self.x = 0
 
     def move(self):
         """Move background a bit"""
