d8fe3875bd3baa35d861090e660ebd6cfc954295
AndreyKovalskiy27 <andreykovalskiy27@gmail.com>
2025-10-23 15:18:32 +0300

    Chance project structure and add more options in settings

--- game/settings.py
+++ game/settings.py
@@ -5,5 +5,5 @@ WINDOW_SIZE = (1080, 800)
 TICK_RATE = 60 # 0 for the max tick rate
 PLAYER_SPEED = 10
 BACKGROUND_SPEED = 1
-BACKGROUND_IMAGE_PATH = "assets/images/background.jpg"
-ICON_IMAGE_PATH = "assets/images/icon.jpg"
+BACKGROUND_IMAGE_PATH = "images/background.jpg"
+ICON_IMAGE_PATH = "images/icon.jpg"
